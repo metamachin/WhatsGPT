0632ff8065185862fef347fd684e80254b2be823
metamachin <metamachin@gmail.com>
2023-03-29 06:18:51 +0530

    testing push

--- server/server.js
+++ server/server.js
@@ -1,49 +1,33 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+const venom = require('venom-bot');
+const { response } = require("./chatgpt");
 
-dotenv.config()
-// console.log(process.env.OPENAI_API_KEY);
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-app.get('/', async (req, res) => {
-  res.status(200).send({
-    message: 'Hello from CodeX!'
+venom
+  .create({
+    session: 'session-name', //name of session
+    multidevice: true // for version not multidevice use false.(default: true)
   })
-})
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
 
-app.post('/', async (req, res) => {
-  try {
-    const prompt = req.body.prompt;
-    console.log(prompt);
-
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
-    res.status(200).send({
-      bot: response.data.choices[0].text
-    });
-
-  } catch (error) {
-    console.error(error)
-    res.status(500).send(error || 'Something went wrong');
-  }
-})
-
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+function start(client) {
+  client.onMessage((message) => {
+// This ignore messages from group and story 
+    if (message.body && message.isGroupMsg === false && message.from !== 'status@broadcast') {
+        response(message.body).then(data => {
+            console.log(data.data.choices[0]);
+            client
+            .sendText(message.from, data.data.choices[0].text)
+            .then((result) => {
+              console.log('Result: ', result); //return object success
+            })
+            .catch((erro) => {
+              console.error('Error when sending: ', erro); //return object error
+            });
+          }).catch(err =>{
+            console.log(err);
+          })
+    }
+  });
+}
